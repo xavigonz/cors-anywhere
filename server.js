3ab85e909c15c19c3abfdcc1943baa4d5d13755b
xavigonz <javier.helly@gmail.com>
2022-01-15 12:15:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: [originWhitelist],
+  originWhitelist: originWhitelist,
   requireHeader: ['origin'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
